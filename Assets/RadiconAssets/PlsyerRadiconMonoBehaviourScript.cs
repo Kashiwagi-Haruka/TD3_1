aa52362637a4807b4ae5be209bee7ae32a101853
Kashiwagi-Haruka <k024g0020@g.neec.ac.jp>
2026-03-11 15:37:08 +0900

    いろいろ

---
+++ Assets/RadiconAssets/PlsyerRadiconMonoBehaviourScript.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlsyerRadiconMonoBehaviourScript : MonoBehaviour
+{
+    public float acceleration = 10f;// Acceleration of the player
+    public float maxSpeed = 5f;// Maximum speed of the player
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
