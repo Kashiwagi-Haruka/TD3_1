17c84ca7ad5de4922158f6ebec2bf5a444eb43ec
Kashiwagi-Haruka <k024g0020@g.neec.ac.jp>
2026-03-11 16:16:00 +0900

    a

--- Assets/RadiconAssets/PlsyerRadiconMonoBehaviourScript.cs
+++ Assets/RadiconAssets/PlsyerRadiconMonoBehaviourScript.cs
@@ -1,19 +1,69 @@
 using UnityEngine;
 
-public class PlsyerRadiconMonoBehaviourScript : MonoBehaviour
-{
-    public float acceleration = 10f;// Acceleration of the player
-    public float maxSpeed = 5f;// Maximum speed of the player
+public class PlsyerRadiconMonoBehaviourScript : MonoBehaviour {
+    [Header("Movement")]
+    public float moveSpeed = 6f;
+    public float gravity = -9.81f;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("Look")]
+    public Transform viewPivot;
+    public float mouseSensitivity = 2f;
+    public float minPitch = -70f;
+    public float maxPitch = 75f;
+    public bool lockCursor = true;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    float pitch;
+    float verticalVelocity;
+    CharacterController characterController;
+
+    void Start () {
+        characterController = GetComponent<CharacterController>();
+
+        if (viewPivot == null && UnityEngine.Camera.main != null) {
+            viewPivot = UnityEngine.Camera.main.transform;
+            }
+
+        if (lockCursor) {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            }
+        }
+
+    void Update () {
+        HandleLook();
+        HandleMove();
+        }
+
+    void HandleLook () {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        transform.Rotate(0f, mouseX, 0f);
+
+        pitch -= mouseY;
+        pitch = Mathf.Clamp(pitch, minPitch, maxPitch);
+
+        if (viewPivot != null) {
+            viewPivot.localRotation = Quaternion.Euler(pitch, 0f, 0f);
+            }
+        }
+
+    void HandleMove () {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Vector3 move = ( transform.right * h + transform.forward * v ).normalized * moveSpeed;
+
+        if (characterController != null) {
+            if (characterController.isGrounded && verticalVelocity < 0f) {
+                verticalVelocity = -2f;
+                }
+
+            verticalVelocity += gravity * Time.deltaTime;
+            Vector3 velocity = move + Vector3.up * verticalVelocity;
+            characterController.Move(velocity * Time.deltaTime);
+            } else {
+            transform.position += move * Time.deltaTime;
+            }
+        }
     }
-}
