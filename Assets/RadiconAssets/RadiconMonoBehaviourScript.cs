c35992e02e228c09cab0d200b2392cee51474e3c
Kashiwagi-Haruka <k024g0020@g.neec.ac.jp>
2026-03-12 22:07:13 +0900

    a

--- Assets/RadiconAssets/RadiconMonoBehaviourScript.cs
+++ Assets/RadiconAssets/RadiconMonoBehaviourScript.cs
@@ -18,16 +18,123 @@ public class RadiconMonoBehaviourScript : MonoBehaviour {
     [SerializeField] private float groundCheckRadius = 0.35f;
     [SerializeField] private LayerMask groundLayers = ~0;
 
+    [Header("Portal")]
+    [SerializeField] private Transform topPortalTarget;
+    [SerializeField] private Transform floatingPortalTarget;
+    [SerializeField] private Vector3 topPortalLocalPosition = new Vector3(0f, 0.6f, 0f);
+    [SerializeField] private Vector3 floatingPortalLocalPosition = new Vector3(0f, 1.4f, 0.25f);
+    [SerializeField] private Vector3 portalLocalScale = new Vector3(0.45f, 0.25f, 1f);
+    [SerializeField] private Vector3 portalForwardCameraLocalPosition = new Vector3(0f, 0.35f, 0.85f);
+    [SerializeField] private float portalCameraFarClipPlane = 80f;
+    [SerializeField] private int portalRenderTextureSize = 512;
+
     private Rigidbody rb;
 
     private void Awake () {
         rb = GetComponent<Rigidbody>();
+        ResolvePortalTargets();
+        EnsurePortalPair();
 
         if (groundCheck == null) {
             groundCheck = transform;
             }
         }
 
+    private void ResolvePortalTargets () {
+        if (topPortalTarget == null) {
+            topPortalTarget = transform;
+            }
+
+        if (floatingPortalTarget == null) {
+            floatingPortalTarget = transform;
+            }
+        }
+
+    private void EnsurePortalPair () {
+        Transform topPortalTransform = transform.Find("TopPortal");
+        Transform floatingPortalTransform = transform.Find("FloatingPortal");
+        Transform portalCameraTransform = transform.Find("PortalForwardCamera");
+
+        GameObject topPortal = topPortalTransform == null
+            ? CreatePortalSurface("TopPortal", topPortalTarget, topPortalLocalPosition)
+            : topPortalTransform.gameObject;
+
+        GameObject floatingPortal = floatingPortalTransform == null
+            ? CreatePortalSurface("FloatingPortal", floatingPortalTarget, floatingPortalLocalPosition)
+            : floatingPortalTransform.gameObject;
+
+        ReattachPortal(topPortal.transform, topPortalTarget, topPortalLocalPosition);
+        ReattachPortal(floatingPortal.transform, floatingPortalTarget, floatingPortalLocalPosition);
+
+        RenderTexture portalRenderTexture = new RenderTexture(portalRenderTextureSize, portalRenderTextureSize, 16);
+        portalRenderTexture.name = $"{name}_PortalRenderTexture";
+
+        BindPortalTexture(topPortal, portalRenderTexture);
+        BindPortalTexture(floatingPortal, portalRenderTexture);
+
+        GameObject portalCameraObject = portalCameraTransform == null
+            ? new GameObject("PortalForwardCamera")
+            : portalCameraTransform.gameObject;
+
+        portalCameraObject.transform.SetParent(transform, false);
+        portalCameraObject.transform.localPosition = portalForwardCameraLocalPosition;
+        portalCameraObject.transform.localRotation = Quaternion.identity;
+
+        UnityEngine.Camera portalCamera = portalCameraObject.GetComponent<UnityEngine.Camera>();
+        if (portalCamera == null) {
+            portalCamera = portalCameraObject.AddComponent<UnityEngine.Camera>();
+            }
+
+        portalCamera.targetTexture = portalRenderTexture;
+        portalCamera.clearFlags = CameraClearFlags.Skybox;
+        portalCamera.nearClipPlane = 0.05f;
+        portalCamera.farClipPlane = portalCameraFarClipPlane;
+        portalCamera.fieldOfView = 65f;
+        }
+
+    private GameObject CreatePortalSurface (string portalName, Transform parentTarget, Vector3 localPosition) {
+        GameObject portalObject = GameObject.CreatePrimitive(PrimitiveType.Quad);
+        portalObject.name = portalName;
+
+        ReattachPortal(portalObject.transform, parentTarget, localPosition);
+
+        Collider portalCollider = portalObject.GetComponent<Collider>();
+        if (portalCollider != null) {
+            Destroy(portalCollider);
+            }
+
+        return portalObject;
+        }
+
+    private void ReattachPortal (Transform portalTransform, Transform parentTarget, Vector3 localPosition) {
+        if (portalTransform == null || parentTarget == null) {
+            return;
+            }
+
+        portalTransform.SetParent(parentTarget, false);
+        portalTransform.localPosition = localPosition;
+        portalTransform.localRotation = Quaternion.Euler(-90f, 0f, 0f);
+        portalTransform.localScale = portalLocalScale;
+        }
+
+    private void BindPortalTexture (GameObject portalObject, RenderTexture renderTexture) {
+        MeshRenderer portalRenderer = portalObject.GetComponent<MeshRenderer>();
+        if (portalRenderer == null) {
+            return;
+            }
+
+        Shader unlitTextureShader = Shader.Find("Universal Render Pipeline/Unlit");
+        if (unlitTextureShader == null) {
+            unlitTextureShader = Shader.Find("Unlit/Texture");
+            }
+
+        Material portalMaterial = unlitTextureShader == null
+            ? new Material(Shader.Find("Standard"))
+            : new Material(unlitTextureShader);
+
+        portalMaterial.mainTexture = renderTexture;
+        portalRenderer.material = portalMaterial;
+        }
     private void FixedUpdate () {
         float throttle = Input.GetAxisRaw("Vertical");
         float steer = Input.GetAxisRaw("Horizontal");
